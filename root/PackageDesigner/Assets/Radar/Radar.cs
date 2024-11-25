2640c02ae5833d5634eb038eb46db1c84292c486
Wyatt Murray <wyattmurray1235@gmail.com>
2024-11-25 16:02:49 -0500

    radar biginnings

---
+++ root/PackageDesigner/Assets/Radar/Radar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    //unity event to declare a hit
+
+    //struct of data about hit
+
+    struct RadarHit
+    {
+
+    }
+
+    //sweeping math
+
+    //math to determine where on radar hit point is
+
+    //collision stuff for collider
+}
