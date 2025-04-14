25f628db9f322b51144692d21203869263dcf7f9
Wyatt Murray <113722636+Skyfall1235@users.noreply.github.com>
2025-04-14 14:37:07 -0400

    remade cutom logging and nameapace definintion

---
+++ root/PackageDesigner/Assets/CustomNamespace/CustomNamespace.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// A comprehensive namespace containing a variety of custom utilities and functionality for Unity development.
+/// It encompasses extensions, generic data structures, input handling, logging, and other miscellaneous tools.
+/// </summary>
+namespace CustomNamespace
+{ }
+
